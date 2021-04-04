3c9a138b77af0e2d59744b78a180894c818e1b7d
wata_mac <watassbass@gmail.com>
2021-04-04 17:20:52 +0900

    Update Magic Modules

--- rules/magicmodules/google_compute_node_template_invalid_cpu_overcommit_type.go
+++ rules/magicmodules/google_compute_node_template_invalid_cpu_overcommit_type.go
@@ -20,47 +20,47 @@ import (
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 )
 
-// GoogleSqlSourceRepresentationInstanceInvalidDatabaseVersionRule checks the pattern is valid
-type GoogleSqlSourceRepresentationInstanceInvalidDatabaseVersionRule struct {
+// GoogleComputeNodeTemplateInvalidCpuOvercommitTypeRule checks the pattern is valid
+type GoogleComputeNodeTemplateInvalidCpuOvercommitTypeRule struct {
 	resourceType  string
 	attributeName string
 }
 
-// NewGoogleSqlSourceRepresentationInstanceInvalidDatabaseVersionRule returns new rule with default attributes
-func NewGoogleSqlSourceRepresentationInstanceInvalidDatabaseVersionRule() *GoogleSqlSourceRepresentationInstanceInvalidDatabaseVersionRule {
-	return &GoogleSqlSourceRepresentationInstanceInvalidDatabaseVersionRule{
-		resourceType:  "google_sql_source_representation_instance",
-		attributeName: "database_version",
+// NewGoogleComputeNodeTemplateInvalidCpuOvercommitTypeRule returns new rule with default attributes
+func NewGoogleComputeNodeTemplateInvalidCpuOvercommitTypeRule() *GoogleComputeNodeTemplateInvalidCpuOvercommitTypeRule {
+	return &GoogleComputeNodeTemplateInvalidCpuOvercommitTypeRule{
+		resourceType:  "google_compute_node_template",
+		attributeName: "cpu_overcommit_type",
 	}
 }
 
 // Name returns the rule name
-func (r *GoogleSqlSourceRepresentationInstanceInvalidDatabaseVersionRule) Name() string {
-	return "google_sql_source_representation_instance_invalid_database_version"
+func (r *GoogleComputeNodeTemplateInvalidCpuOvercommitTypeRule) Name() string {
+	return "google_compute_node_template_invalid_cpu_overcommit_type"
 }
 
 // Enabled returns whether the rule is enabled by default
-func (r *GoogleSqlSourceRepresentationInstanceInvalidDatabaseVersionRule) Enabled() bool {
+func (r *GoogleComputeNodeTemplateInvalidCpuOvercommitTypeRule) Enabled() bool {
 	return true
 }
 
 // Severity returns the rule severity
-func (r *GoogleSqlSourceRepresentationInstanceInvalidDatabaseVersionRule) Severity() string {
+func (r *GoogleComputeNodeTemplateInvalidCpuOvercommitTypeRule) Severity() string {
 	return tflint.ERROR
 }
 
 // Link returns the rule reference link
-func (r *GoogleSqlSourceRepresentationInstanceInvalidDatabaseVersionRule) Link() string {
+func (r *GoogleComputeNodeTemplateInvalidCpuOvercommitTypeRule) Link() string {
 	return ""
 }
 
 // Check checks the pattern is valid
-func (r *GoogleSqlSourceRepresentationInstanceInvalidDatabaseVersionRule) Check(runner tflint.Runner) error {
+func (r *GoogleComputeNodeTemplateInvalidCpuOvercommitTypeRule) Check(runner tflint.Runner) error {
 	return runner.WalkResourceAttributes(r.resourceType, r.attributeName, func(attribute *hcl.Attribute) error {
 		var val string
 		err := runner.EvaluateExpr(attribute.Expr, &val, nil)
 
-		validateFunc := validation.StringInSlice([]string{"MYSQL_5_5", "MYSQL_5_6", "MYSQL_5_7", "MYSQL_8_0"}, false)
+		validateFunc := validation.StringInSlice([]string{"ENABLED", "NONE", ""}, false)
 
 		return runner.EnsureNoError(err, func() error {
 			_, errors := validateFunc(val, r.attributeName)
