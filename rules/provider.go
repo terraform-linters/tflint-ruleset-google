51b9f23033e0cf4f27de63f13f84620d58dba00e
wata_mac <watassbass@gmail.com>
2022-04-04 02:17:42 +0900

    Fix rule template for rule generator

--- rules/provider.go
+++ rules/provider.go
@@ -6,30 +6,26 @@ import (
 	"github.com/terraform-linters/tflint-ruleset-google/rules/magicmodules"
 )
 
-var rules = [][]tflint.Rule{
-	{
-		NewGoogleComposerEnvironmentInvalidMachineTypeRule(),
-		NewGoogleComputeInstanceInvalidMachineTypeRule(),
-		NewGoogleComputeReservationInvalidMachineTypeRule(),
-		NewGoogleComputeInstanceTemplateInvalidMachineTypeRule(),
-		NewGoogleContainerClusterInvalidMachineTypeRule(),
-		NewGoogleContainerNodePoolInvalidMachineTypeRule(),
-		NewGoogleDataflowJobInvalidMachineTypeRule(),
-		NewGoogleComputeResourcePolicyInvalidNameRule(),
-		NewGoogleProjectIamMemberInvalidMemberRule(),
-		NewGoogleProjectIamAuditConfigInvalidMemberRule(),
-		NewGoogleProjectIamBindingInvalidMemberRule(),
-		NewGoogleProjectIamPolicyInvalidMemberRule(),
-	},
-	magicmodules.Rules,
-	api.Rules,
+var manualRules = []tflint.Rule{
+	NewGoogleComposerEnvironmentInvalidMachineTypeRule(),
+	NewGoogleComputeInstanceInvalidMachineTypeRule(),
+	NewGoogleComputeReservationInvalidMachineTypeRule(),
+	NewGoogleComputeInstanceTemplateInvalidMachineTypeRule(),
+	NewGoogleContainerClusterInvalidMachineTypeRule(),
+	NewGoogleContainerNodePoolInvalidMachineTypeRule(),
+	NewGoogleDataflowJobInvalidMachineTypeRule(),
+	NewGoogleComputeResourcePolicyInvalidNameRule(),
+	NewGoogleProjectIamMemberInvalidMemberRule(),
+	NewGoogleProjectIamAuditConfigInvalidMemberRule(),
+	NewGoogleProjectIamBindingInvalidMemberRule(),
+	NewGoogleProjectIamPolicyInvalidMemberRule(),
 }
 
 // Rules is a list of all rules
 var Rules []tflint.Rule
 
 func init() {
-	for _, r := range rules {
-		Rules = append(Rules, r...)
-	}
+	Rules = append(Rules, manualRules...)
+	Rules = append(Rules, magicmodules.Rules...)
+	Rules = append(Rules, api.Rules...)
 }
