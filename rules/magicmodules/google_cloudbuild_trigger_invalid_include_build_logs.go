0ab741971fc00e54c971bc8d895be8cdd9110373
Kazuma Watanabe <watassbass@gmail.com>
2022-06-19 00:02:26 +0900

    Update Magic Modules (#185)
    * Change submodule url to https
    
    * Update Magic Modules

--- rules/magicmodules/google_cloudbuild_trigger_invalid_include_build_logs.go
+++ rules/magicmodules/google_cloudbuild_trigger_invalid_include_build_logs.go
@@ -20,44 +20,44 @@ import (
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 )
 
-// GoogleComputeSubnetworkInvalidIpv6AccessTypeRule checks the pattern is valid
-type GoogleComputeSubnetworkInvalidIpv6AccessTypeRule struct {
+// GoogleCloudbuildTriggerInvalidIncludeBuildLogsRule checks the pattern is valid
+type GoogleCloudbuildTriggerInvalidIncludeBuildLogsRule struct {
 	tflint.DefaultRule
 
 	resourceType  string
 	attributeName string
 }
 
-// NewGoogleComputeSubnetworkInvalidIpv6AccessTypeRule returns new rule with default attributes
-func NewGoogleComputeSubnetworkInvalidIpv6AccessTypeRule() *GoogleComputeSubnetworkInvalidIpv6AccessTypeRule {
-	return &GoogleComputeSubnetworkInvalidIpv6AccessTypeRule{
-		resourceType:  "google_compute_subnetwork",
-		attributeName: "ipv6_access_type",
+// NewGoogleCloudbuildTriggerInvalidIncludeBuildLogsRule returns new rule with default attributes
+func NewGoogleCloudbuildTriggerInvalidIncludeBuildLogsRule() *GoogleCloudbuildTriggerInvalidIncludeBuildLogsRule {
+	return &GoogleCloudbuildTriggerInvalidIncludeBuildLogsRule{
+		resourceType:  "google_cloudbuild_trigger",
+		attributeName: "include_build_logs",
 	}
 }
 
 // Name returns the rule name
-func (r *GoogleComputeSubnetworkInvalidIpv6AccessTypeRule) Name() string {
-	return "google_compute_subnetwork_invalid_ipv6_access_type"
+func (r *GoogleCloudbuildTriggerInvalidIncludeBuildLogsRule) Name() string {
+	return "google_cloudbuild_trigger_invalid_include_build_logs"
 }
 
 // Enabled returns whether the rule is enabled by default
-func (r *GoogleComputeSubnetworkInvalidIpv6AccessTypeRule) Enabled() bool {
+func (r *GoogleCloudbuildTriggerInvalidIncludeBuildLogsRule) Enabled() bool {
 	return true
 }
 
 // Severity returns the rule severity
-func (r *GoogleComputeSubnetworkInvalidIpv6AccessTypeRule) Severity() tflint.Severity {
+func (r *GoogleCloudbuildTriggerInvalidIncludeBuildLogsRule) Severity() tflint.Severity {
 	return tflint.ERROR
 }
 
 // Link returns the rule reference link
-func (r *GoogleComputeSubnetworkInvalidIpv6AccessTypeRule) Link() string {
+func (r *GoogleCloudbuildTriggerInvalidIncludeBuildLogsRule) Link() string {
 	return ""
 }
 
 // Check checks the pattern is valid
-func (r *GoogleComputeSubnetworkInvalidIpv6AccessTypeRule) Check(runner tflint.Runner) error {
+func (r *GoogleCloudbuildTriggerInvalidIncludeBuildLogsRule) Check(runner tflint.Runner) error {
 	resources, err := runner.GetResourceContent(r.resourceType, &hclext.BodySchema{
 		Attributes: []hclext.AttributeSchema{{Name: r.attributeName}},
 	}, nil)
@@ -74,7 +74,7 @@ func (r *GoogleComputeSubnetworkInvalidIpv6AccessTypeRule) Check(runner tflint.R
 		var val string
 		err := runner.EvaluateExpr(attribute.Expr, &val, nil)
 
-		validateFunc := validation.StringInSlice([]string{"EXTERNAL", "INTERNAL", ""}, false)
+		validateFunc := validation.StringInSlice([]string{"INCLUDE_BUILD_LOGS_UNSPECIFIED", "INCLUDE_BUILD_LOGS_WITH_STATUS", ""}, false)
 
 		err = runner.EnsureNoError(err, func() error {
 			_, errors := validateFunc(val, r.attributeName)
