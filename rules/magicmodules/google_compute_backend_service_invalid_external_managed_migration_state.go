5efdf8dcb1cf9f5eb81b25f087726f05668c7c1c
wata727 <9624059+wata727@users.noreply.github.com>
2025-05-19 01:59:15 +0000

    Update Magic Modules

--- rules/magicmodules/google_compute_backend_service_invalid_external_managed_migration_state.go
+++ rules/magicmodules/google_compute_backend_service_invalid_external_managed_migration_state.go
@@ -20,44 +20,44 @@ import (
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 )
 
-// GoogleComputeRegionBackendServiceInvalidProtocolRule checks the pattern is valid
-type GoogleComputeRegionBackendServiceInvalidProtocolRule struct {
+// GoogleComputeBackendServiceInvalidExternalManagedMigrationStateRule checks the pattern is valid
+type GoogleComputeBackendServiceInvalidExternalManagedMigrationStateRule struct {
 	tflint.DefaultRule
 
 	resourceType  string
 	attributeName string
 }
 
-// NewGoogleComputeRegionBackendServiceInvalidProtocolRule returns new rule with default attributes
-func NewGoogleComputeRegionBackendServiceInvalidProtocolRule() *GoogleComputeRegionBackendServiceInvalidProtocolRule {
-	return &GoogleComputeRegionBackendServiceInvalidProtocolRule{
-		resourceType:  "google_compute_region_backend_service",
-		attributeName: "protocol",
+// NewGoogleComputeBackendServiceInvalidExternalManagedMigrationStateRule returns new rule with default attributes
+func NewGoogleComputeBackendServiceInvalidExternalManagedMigrationStateRule() *GoogleComputeBackendServiceInvalidExternalManagedMigrationStateRule {
+	return &GoogleComputeBackendServiceInvalidExternalManagedMigrationStateRule{
+		resourceType:  "google_compute_backend_service",
+		attributeName: "external_managed_migration_state",
 	}
 }
 
 // Name returns the rule name
-func (r *GoogleComputeRegionBackendServiceInvalidProtocolRule) Name() string {
-	return "google_compute_region_backend_service_invalid_protocol"
+func (r *GoogleComputeBackendServiceInvalidExternalManagedMigrationStateRule) Name() string {
+	return "google_compute_backend_service_invalid_external_managed_migration_state"
 }
 
 // Enabled returns whether the rule is enabled by default
-func (r *GoogleComputeRegionBackendServiceInvalidProtocolRule) Enabled() bool {
+func (r *GoogleComputeBackendServiceInvalidExternalManagedMigrationStateRule) Enabled() bool {
 	return true
 }
 
 // Severity returns the rule severity
-func (r *GoogleComputeRegionBackendServiceInvalidProtocolRule) Severity() tflint.Severity {
+func (r *GoogleComputeBackendServiceInvalidExternalManagedMigrationStateRule) Severity() tflint.Severity {
 	return tflint.ERROR
 }
 
 // Link returns the rule reference link
-func (r *GoogleComputeRegionBackendServiceInvalidProtocolRule) Link() string {
+func (r *GoogleComputeBackendServiceInvalidExternalManagedMigrationStateRule) Link() string {
 	return ""
 }
 
 // Check checks the pattern is valid
-func (r *GoogleComputeRegionBackendServiceInvalidProtocolRule) Check(runner tflint.Runner) error {
+func (r *GoogleComputeBackendServiceInvalidExternalManagedMigrationStateRule) Check(runner tflint.Runner) error {
 	resources, err := runner.GetResourceContent(r.resourceType, &hclext.BodySchema{
 		Attributes: []hclext.AttributeSchema{{Name: r.attributeName}},
 	}, nil)
@@ -72,7 +72,7 @@ func (r *GoogleComputeRegionBackendServiceInvalidProtocolRule) Check(runner tfli
 		}
 
 		err := runner.EvaluateExpr(attribute.Expr, func(val string) error {
-			validateFunc := validation.StringInSlice([]string{"HTTP", "HTTPS", "HTTP2", "TCP", "SSL", "UDP", "GRPC", "UNSPECIFIED", "H2C", ""}, false)
+			validateFunc := validation.StringInSlice([]string{"PREPARE", "TEST_BY_PERCENTAGE", "TEST_ALL_TRAFFIC", ""}, false)
 
 			_, errors := validateFunc(val, r.attributeName)
 			for _, err := range errors {
