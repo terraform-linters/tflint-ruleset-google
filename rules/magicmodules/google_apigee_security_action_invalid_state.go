413b0fdff00a89330e353de3ff1ca47858a7b2f5
wata727 <9624059+wata727@users.noreply.github.com>
2025-07-28 02:12:19 +0000

    Update Magic Modules

--- rules/magicmodules/google_apigee_security_action_invalid_state.go
+++ rules/magicmodules/google_apigee_security_action_invalid_state.go
@@ -20,44 +20,44 @@ import (
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 )
 
-// GoogleVmwareengineNetworkPeeringInvalidPeerNetworkTypeRule checks the pattern is valid
-type GoogleVmwareengineNetworkPeeringInvalidPeerNetworkTypeRule struct {
+// GoogleApigeeSecurityActionInvalidStateRule checks the pattern is valid
+type GoogleApigeeSecurityActionInvalidStateRule struct {
 	tflint.DefaultRule
 
 	resourceType  string
 	attributeName string
 }
 
-// NewGoogleVmwareengineNetworkPeeringInvalidPeerNetworkTypeRule returns new rule with default attributes
-func NewGoogleVmwareengineNetworkPeeringInvalidPeerNetworkTypeRule() *GoogleVmwareengineNetworkPeeringInvalidPeerNetworkTypeRule {
-	return &GoogleVmwareengineNetworkPeeringInvalidPeerNetworkTypeRule{
-		resourceType:  "google_vmwareengine_network_peering",
-		attributeName: "peer_network_type",
+// NewGoogleApigeeSecurityActionInvalidStateRule returns new rule with default attributes
+func NewGoogleApigeeSecurityActionInvalidStateRule() *GoogleApigeeSecurityActionInvalidStateRule {
+	return &GoogleApigeeSecurityActionInvalidStateRule{
+		resourceType:  "google_apigee_security_action",
+		attributeName: "state",
 	}
 }
 
 // Name returns the rule name
-func (r *GoogleVmwareengineNetworkPeeringInvalidPeerNetworkTypeRule) Name() string {
-	return "google_vmwareengine_network_peering_invalid_peer_network_type"
+func (r *GoogleApigeeSecurityActionInvalidStateRule) Name() string {
+	return "google_apigee_security_action_invalid_state"
 }
 
 // Enabled returns whether the rule is enabled by default
-func (r *GoogleVmwareengineNetworkPeeringInvalidPeerNetworkTypeRule) Enabled() bool {
+func (r *GoogleApigeeSecurityActionInvalidStateRule) Enabled() bool {
 	return true
 }
 
 // Severity returns the rule severity
-func (r *GoogleVmwareengineNetworkPeeringInvalidPeerNetworkTypeRule) Severity() tflint.Severity {
+func (r *GoogleApigeeSecurityActionInvalidStateRule) Severity() tflint.Severity {
 	return tflint.ERROR
 }
 
 // Link returns the rule reference link
-func (r *GoogleVmwareengineNetworkPeeringInvalidPeerNetworkTypeRule) Link() string {
+func (r *GoogleApigeeSecurityActionInvalidStateRule) Link() string {
 	return ""
 }
 
 // Check checks the pattern is valid
-func (r *GoogleVmwareengineNetworkPeeringInvalidPeerNetworkTypeRule) Check(runner tflint.Runner) error {
+func (r *GoogleApigeeSecurityActionInvalidStateRule) Check(runner tflint.Runner) error {
 	resources, err := runner.GetResourceContent(r.resourceType, &hclext.BodySchema{
 		Attributes: []hclext.AttributeSchema{{Name: r.attributeName}},
 	}, nil)
@@ -72,7 +72,7 @@ func (r *GoogleVmwareengineNetworkPeeringInvalidPeerNetworkTypeRule) Check(runne
 		}
 
 		err := runner.EvaluateExpr(attribute.Expr, func(val string) error {
-			validateFunc := validation.StringInSlice([]string{"STANDARD", "VMWARE_ENGINE_NETWORK", "PRIVATE_SERVICES_ACCESS", "NETAPP_CLOUD_VOLUMES", "THIRD_PARTY_SERVICE", "DELL_POWERSCALE", "GOOGLE_CLOUD_NETAPP_VOLUMES"}, false)
+			validateFunc := validation.StringInSlice([]string{"ENABLED", "DISABLED"}, false)
 
 			_, errors := validateFunc(val, r.attributeName)
 			for _, err := range errors {
